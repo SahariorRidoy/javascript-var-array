12deeca3e86a6b5589d8c976f634113714f772f3
Saharior Ridoy <emonhridoy2014@gmail.com>
2024-08-30 22:23:06 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,54 @@
+
+
+
+
+// var balance =1000;
+// var orange =200;
+// var apples =500;
+// var total=(balance - orange - apples);
+// console.log(total);
+
+
+
+
+// var one=75.25;
+// var two =65;
+// var three = 80;
+// var four =35.45;
+// var five = 99.50;
+// var sum=(one + two + three + four + five);
+// var avg=(sum/5 );
+
+
+
+
+
+// console.log ( avg.toFixed(2));
+// var avg= parseFloat()
+
+// const a= isNaN(2-10);
+// console.log(a);
+
+
+
+
+// let a=8;
+// if(a<8){
+//     console.log("Number is less than 8");
+// }
+// else if(a==8){
+//     console.log("Number is equal to 8");
+// }
+// else{
+//     console.log("Number is greater than 8");
+// }
+
+
+
+const number= [12,22,54,65];
+console.log(number);
+
+const name= ["abul",'bulbul','babul',30,'lablu',45];
+console.log(name[2]);
+name[2]='habul';
+console.log(name);
